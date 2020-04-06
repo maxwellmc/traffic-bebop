52be9f9e303b6d0e2fc172e7b456d5a15179466c
Max McMahon <wmaxmcmahon@gmail.com>
2020-04-06 11:17:28 -0400

    Restructure directories

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     watch: true,
-    entry: './assets/js/game.js',
+    entry: './src/game.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
